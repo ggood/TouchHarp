fc7180bd0daf45d69cf895d0a1c145f236f0aaa4
Gordon Good <velo27@yahoo.com>
2014-03-02 02:53:28 -0800

    Add comments, remove refefences to LED1-3 (leftovers from initial prototype)

--- TouchHarpMultiplexer.ino
+++ TouchHarpMultiplexer.ino
@@ -1,11 +1,52 @@
+/**
+
+The Touch Harp is an instrument played like a harp, but uses
+touch sensitive input on a Teensy 3 microcontroller.
+
+The Teensy3 has 9 pins that can do touch sensing input. Since
+a traditional harp has many more strings, we need some way
+to allow more than 9 "strings" to be sensed.
+
+An analog multiplexer chip like to CD4067 (1 x 16) allows one
+touch input pin on the Teensy to handle 16 separate touch
+inputs if you periodically select each input and record
+the current value. To select which one of the 16 inputs on
+the 4067 is active, you need to write values to the four
+select pins on the chip, e.g.
+
+S3 S2 S1 S0
+ 0  0  0  0 - input 0 is selected
+ 0  0  0  1 - input 1 is selected
+ 0  0  1  0 - input 2 is selected
+
+and so on, up to
+
+ 1  1  1  1 - input 15 is selected
+ 
+This skectch assumes the following connections between the
+Teensy 3.0 pins and the analog multiplexer:
+
+Teensy  4067
+======  ====
+     2    10
+     3    11
+     4    14
+     5    13
+
+A datasheet for the 4067 is available online at:
+https://www.sparkfun.com/datasheets/IC/CD74HC4067.pdf
+
+So the basic idea is to select an in
+*/
+
+
+
+
 // Touch input pin
 #define TOUCH_IN A9
 
 // LED output pins
-#define LED0 6
-#define LED1 7
-#define LED2 8
-#define LED3 9
+#define LED0 13
 
 // Multiplexer select pins
 #define SEL0 2
@@ -90,8 +131,11 @@ void TouchPin::update() {
       digitalWrite(SEL2, (_select_line >> 2)  & 0x01);
       digitalWrite(SEL3, (_select_line >> 3)  & 0x01);
     }
+    // Remember the time we read this value
     _last_sample_time = millis();
+    // Read the value
     unsigned int val = touchRead(_pin);
+    // Update the running average
     _sum -= _samples[_index];
     _samples[_index] = val;
     _sum += val;
@@ -100,25 +144,37 @@ void TouchPin::update() {
 }
 
 unsigned int TouchPin::value() {
+  // Return the running average
   return _sum / SAMPLE_BUFFER_SIZE;
 }
 
 boolean TouchPin::down() {
+  // If the current value is above the touch threshold, return true
   return value() > _touch_threshold;
 }
 
 void TouchPin::set_touch_threshold(unsigned int touch_threshold) {
+  // Set the threshold value - if the value we read is larger than
+  // touch_threshold, we report a touch event.
   _touch_threshold = touch_threshold;
 }
 
 
 class HarpString: public TouchPin {
+  /**
+  A HarpString extends the TouchPin object and adds the concept of a "pluck"
+  operation. A pluck happens when the string is touched (we enter the "armed"
+  state) and them is released.
+  
+  TODO(ggood) implement the plucked() method
+  */
 public:
   HarpString(int pin, unsigned long sample_period) : TouchPin(pin, sample_period) {};
   HarpString(int pin, unsigned long sample_period, unsigned int select_line) : TouchPin(pin, sample_period, select_line) {};
 };
 
-
+// The current prototype has 13 "strings", all attached to
+// analog input 9 on the Teensy
 TouchPin strings[13] = {
   HarpString(A9, SAMPLE_PERIOD, 0),
   HarpString(A9, SAMPLE_PERIOD, 1),
@@ -205,9 +261,6 @@ void setup() {
 
   // Set LED output pins
   pinMode(LED0, OUTPUT);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
 
   // Select pins for multiplexer
   pinMode(SEL0, OUTPUT);
